ba400a1483d347e4f4c2b9234df59dfb786797fd
ToilaBao <thienbaodx2000@gmail.com>
2020-07-10 15:17:40 +0700

    1st: first project upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# Welcome to ToilaBao repository
+This repository contains a simple Java application used to demonstrate the CI - Continuous Integration process, a part of Introduction to Software Engineering course at FPT University, HCMC Campus
+## You will find in this repo:
+* MathUtil class: holds all of our own Math methods
+* Test class: demostrate on how to ensure the software quality. The tecnics applied: TDD, Unit Testing (JUnit)
+* .gitignore
+* Markdown file: this file itself
+
+## Connect me via:
+1. [GitHub](https://github.com/ToilaBao)
+2. [Facebook](https://facebook.com/bao.thien.nguyen.thanh.291)
+
+### © by ToilaBao
---
+++ src/bao/util/MathUtil.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bao.util;
+
+/**
+ *
+ * @author Bao
+ */
+//đây là class chứa các hàm toán học ta tự viết, mục đích là công cụ 
+//xài chung cho mọi nơi. Phàm cái gì mà đc thiết kế để xài chung
+//ta dùng kĩ thuật STATIC
+public class MathUtil {
+
+    //tính n!=1.2.3...n vì n! tăng rất nhanh, sẽ tràn int sớm
+    //int chỉ lưu tối đa 2 tỷ 1, nên mình xài long an toàn hơn
+    //15! to lắm rồi, tính tối đa 15!
+    public static long computeFactorial(int n) {
+
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        } 
+        return result;
+    }
+}
